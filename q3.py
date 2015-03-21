4d0cdec9960b46eeb90fa1c511b4eb80719e691f
ankush <arjun1177arjun@gmail.com>
2015-03-21 22:15:42 +0530

    q3 done

---
+++ q3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+def end_other(string1,string2):
+  l1=len(string1)
+  l2=len(string2)
+  if l1<l2:
+    s1,s2=string1.lower(),string2.lower()
+  else:
+    s2,s1=string1.lower(),string2.lower()
+  l1=len(s1)
+  l2=len(s2)
+  if(s1==s2[l2-l1:]):
+    return True
+  else:
+    return False
+
