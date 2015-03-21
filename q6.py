13b3f28a85dac0ea6c073f8906b1e1e4e4378fdc
ankush <arjun1177arjun@gmail.com>
2015-03-22 01:30:39 +0530

    DONEgit add q6.py q7.pygit add q6.py q7.pygit add q6.py q7.pygit add q6.py q7.py

--- q6.py
+++ q6.py
@@ -1,24 +1,11 @@
 #!/usr/bin/python
-def lcm(a,b):
-  l=1
-  def prime(n):
-    print n
-    c=0
-    for i in range(1,n):
-      if n%i==0 :
-        c+=1
-    if c==1:
-      return True
-    else:
-      return False
-  i=2
-  while(a>0 or b>0):
-    while(prime(i)==False):
-      i+=1
-    while(a%i==0 or b%i==0):
-      l=l*i
-      if a%i==0:
-        a=a/i
-      if b%i==0:
-        b=b/i
-  print l
+def lcm(x,y):
+  a,b=x,y
+  hcf= min(a,b)
+  while(not(b%hcf==0)):
+    c=hcf
+    hcf=b/hcf
+    b=c
+  lcm=(x*y)/hcf
+  print lcm
+
