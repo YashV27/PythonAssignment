6a07520130579834157f8c06fbff8460bd5b93d5
ankush <arjun1177arjun@gmail.com>
2015-03-21 21:56:25 +0530

    q1 edited

--- q1.py
+++ q1.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 def make_bricks(small,big,goal):
   if ((goal%big)-small)<=goal :
-    return True
+    print True
   else:
-    return False
+    print False
+small=int(raw_input("no. of small bricks\n"))
+big=int(raw_input("no. of big bricks\n"))
+goal=int(raw_input("Enter goal\n"))
+make_bricks(small,big,goal)
